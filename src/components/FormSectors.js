2751a22f896e59e8a3d4ac406ae8e81793c95b35
Shahrear-ahamed <its.shahrear@gmail.com>
2023-01-13 16:40:46 +0600

    form are added and sector are load from database

---
+++ src/components/FormSectors.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+function FormSectors() {
+  const [formSectors, setFormSectors] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/sectors")
+      .then((res) => res.json())
+      .then((data) => setFormSectors(data.formSectors));
+  }, []);
+
+  return formSectors.map(({ value, label, children }) => (
+    <optgroup className="form-group" key={value} label={label}>
+      {children.map((opt) =>
+        // nested options are here
+        opt?.children ? (
+          <Fragment key={opt.value}>
+            <option key={opt.value} value={opt.value} disabled>
+              {opt.label}
+            </option>
+
+            {opt?.children.map((opt2) =>
+              // nested of nested options are here
+
+              opt2?.children ? (
+                <Fragment key={opt2.value}>
+                  <option key={opt2.value} value={opt2.value} disabled>
+                    &nbsp;&nbsp;&nbsp;&nbsp; {opt2.label}
+                  </option>
+
+                  {opt2.children.map((opt3) => (
+                    <option key={opt3.value} value={opt3.value}>
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      {opt3.label}
+                    </option>
+                  ))}
+                </Fragment>
+              ) : (
+                <option key={opt2.value} value={opt2.value}>
+                  &nbsp;&nbsp;&nbsp;&nbsp; {opt2.label}
+                </option>
+              )
+            )}
+          </Fragment>
+        ) : (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        )
+      )}
+    </optgroup>
+  ));
+}
+
+export default FormSectors;
